15f582781b491a7d3c0d65a48becdd8e865ac1e2
Madde <Madde.frey@gmail.com>
2021-10-21 15:23:52 +0000

    Add eventlistener to choice buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,41 @@ const questionBank = [
         choiceD: '1yesno',
         correctAnswer: 'choiceA'
     }, 
+    {
+        question: '6What are you?',
+        choiceA: '1yes',
+        choiceB: '1no',
+        choiceC: '1noyes',
+        choiceD: '1yesno',
+        correctAnswer: 'choiceA'
+    }, 
+    {
+        question: '7What are you?',
+        choiceA: '1yes',
+        choiceB: '1no',
+        choiceC: '1noyes',
+        choiceD: '1yesno',
+        correctAnswer: 'choiceA'
+    } 
 ]
 
-const startButton = document.getElementById('play-btn')
+const playButton = document.getElementById('play-btn')
 const nextButton = document.getElementById('next')
-startButton.addEventListener('click', startGame)
+playButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', getNextQuestion)
+
+// choices here (buttons) + eventlistener
+
+const firstChoiceBtn = document.getElementById('firstBtn');
+const secondChoiceBtn = document.getElementById('secondBtn');
+const thirdChoiceBtn = document.getElementById('thirdBtn');
+const fourthChoiceBtn = document.getElementById('fourthBtn');
+
+firstChoiceBtn.addEventListener('click', checkAnswer);
+secondChoiceBtn.addEventListener('click', checkAnswer);
+thirdChoiceBtn.addEventListener('click', checkAnswer);
+fourthChoiceBtn.addEventListener('click', checkAnswer);
+
 
 let i = 0;
 let currentQuestionIndex = i;
@@ -71,13 +100,23 @@ getNextQuestion()
 
  function getNextQuestion() { 
 
-i++;
-
-document.getElementById('questions').innerHTML = questionBank[i].question;
-document.getElementById('choiceA').innerHTML = questionBank[i].choiceA;
-document.getElementById('choiceB').innerHTML = questionBank[i].choiceB;
-document.getElementById('choiceC').innerHTML = questionBank[i].choiceC;
-document.getElementById('choiceD').innerHTML = questionBank[i].choiceD;
+    if(i < questionBank.length) {
+        do{
+            i++;
+    
+    document.getElementById('questions').innerHTML = questionBank[i].question;
+    document.getElementById('choiceA').innerHTML = questionBank[i].choiceA;
+    document.getElementById('choiceB').innerHTML = questionBank[i].choiceB;
+    document.getElementById('choiceC').innerHTML = questionBank[i].choiceC;
+    document.getElementById('choiceD').innerHTML = questionBank[i].choiceD;
+    
+    
+        }while (i < questionBank.length && document.getElementById('next').clicked)
+    } else if (i = questionBank.length){
+      
+        result();
+    }
+   
 }
 
 
