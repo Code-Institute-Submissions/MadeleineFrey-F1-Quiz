def37be9d42e418672cb55dc0102a72fabd020be
Madde <Madde.frey@gmail.com>
2021-10-23 20:37:16 +0000

    Add new style to game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,59 @@
-//change topic to F1
+
 const questionBank = [
     {
-        question: '1What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
+        question: 'Which driver raced the most laps in 2020?',
+        choiceA: 'Lando Norris',
+        choiceB: 'Charles Leclerc',
+        choiceC: 'Max Verstappen',
+        choiceD: 'Daniel Ricciardo',
         correctAnswer: 'A'
     },
     {
-        question: '2What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
-        correctAnswer: 'A'
+        question: 'What year did Michael Schumacher retire from F1?',
+        choiceA: '2008',
+        choiceB: '2012',
+        choiceC: '2011',
+        choiceD: '2017',
+        correctAnswer: 'B'
     },
     {
-        question: '3What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
+        question: 'What does the red flag mean?',
+        choiceA: 'Warns racing drivers of a slow moving car on track',
+        choiceB: 'Indicate start or end of the race',
+        choiceC: 'All clear, you can continue with race',
+        choiceD: 'Indicates to the drivers that a session or race has been stopped',
         correctAnswer: 'D'
     },
     {
-        question: '4What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
+        question: 'How many world titles did Michael Schumacher win?',
+        choiceA: '3',
+        choiceB: '8',
+        choiceC: '7',
+        choiceD: '4',
         correctAnswer: 'C'
     },
     {
-        question: '5What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
+        question: 'At what race did McLaren make their F1 debut in 1966?',
+        choiceA: 'Hungary',
+        choiceB: ' Monaco',
+        choiceC: 'Italy',
+        choiceD: 'Turkey',
         correctAnswer: 'B'
     }, 
     {
-        question: '6What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
-        correctAnswer: 'B'
+        question: 'What does drs stand for?',
+        choiceA: 'Drag Reduction System',
+        choiceB: 'Direct Registration System',
+        choiceC: 'Driving Reduction System',
+        choiceD: 'Dust Removal System',
+        correctAnswer: 'A'
     }, 
     {
-        question: '7What are you?',
-        choiceA: '1yes',
-        choiceB: '1no',
-        choiceC: '1noyes',
-        choiceD: '1yesno',
+        question: 'Which position is pole position?',
+        choiceA: 'Last',
+        choiceB: 'Second',
+        choiceC: 'First',
+        choiceD: 'Third',
         correctAnswer: 'C'
     } 
 ]
@@ -65,10 +65,10 @@ const questionBank = [
 
 // choices here (buttons) + eventlistener
 
-const firstChoiceBtn = document.getElementById('firstBtn');
-const secondChoiceBtn = document.getElementById('secondBtn');
-const thirdChoiceBtn = document.getElementById('thirdBtn');
-const fourthChoiceBtn = document.getElementById('fourthBtn');
+// const firstChoiceBtn = document.getElementById('firstBtn');
+// const secondChoiceBtn = document.getElementById('secondBtn');
+// const thirdChoiceBtn = document.getElementById('thirdBtn');
+// const fourthChoiceBtn = document.getElementById('fourthBtn');
 
 // firstChoiceBtn.addEventListener('click', checkAnswer);
 // secondChoiceBtn.addEventListener('click', checkAnswer);
@@ -77,7 +77,6 @@ const fourthChoiceBtn = document.getElementById('fourthBtn');
 
 
 let i = 0;
-// let currentQuestionIndex = i;
 let maxQuestion = 0;
 
 
@@ -87,10 +86,6 @@ document.getElementById('choiceA').innerHTML = questionBank[i].choiceA;
 document.getElementById('choiceB').innerHTML = questionBank[i].choiceB;
 document.getElementById('choiceC').innerHTML = questionBank[i].choiceC;
 document.getElementById('choiceD').innerHTML = questionBank[i].choiceD;
-
-//Do comments on the function
-//Remove consol.log() later
-//username if you have time
 
 
 function startGame() {
@@ -116,7 +111,7 @@ document.getElementById('choiceD').innerHTML = questionBank[i].choiceD;
 function jumpToLastQuestion() {
 
     while(maxQuestion < questionBank.length -1){
-        console.log("loop")
+        console.log('loop')
         getNextQuestion(); 
     }
     result();
@@ -127,7 +122,7 @@ function jumpToLastQuestion() {
     console.log('Räknar upp??')
 if(maxQuestion == questionBank.length -1) {
 
-    console.log("innan result")
+    console.log('innan result')
     result();
 
  } else {
@@ -140,7 +135,7 @@ if(maxQuestion == questionBank.length -1) {
     document.getElementById('choiceC').innerHTML = questionBank[i].choiceC;
     document.getElementById('choiceD').innerHTML = questionBank[i].choiceD;
 
-    console.log("räknar upp")
+    console.log('räknar upp')
     maxQuestion++;
 
     console.log(maxQuestion)
@@ -152,14 +147,14 @@ if(maxQuestion == questionBank.length -1) {
 
 function checkAnswer(event) {
 
-    let userAnswer = event.target.getAttribute("data-name");
+    let userAnswer = event.target.getAttribute('data-name');
     console.log(userAnswer);
 
     if (userAnswer == questionBank[i].correctAnswer){
-        console.log("Rätttt")
+        console.log('Rätttt')
         rightAnswer();
     } else {
-        console.log("Felllll")
+        console.log('Felllll')
         wrongAnswer();
     }
 
@@ -178,14 +173,13 @@ function wrongAnswer()
     document.getElementById('incorrect').innerHTML++
 }
 
-// show result in a div?
 function result() {
 
     console.log("DEN LYSSNAR")
 
     Swal.fire( {
         title: 'Good Job! you got',
-        text: document.getElementById('score').innerHTML + ' / 10',
+        text: document.getElementById('score').innerHTML + ' / 7',
         showCancelButton: true,
         }).then((result) => {
             if(result.isConfirmed) {
